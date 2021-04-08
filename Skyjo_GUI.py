cfa05241b35da03808e2a773ce683d2a327802fa
Shriukan33 <benja.mourgues@hotmail.fr>
2021-04-08 21:10:25 +0200

    Improved on readability, added responsive for player surface, added requirements.txt

--- Skyjo_GUI.py
+++ Skyjo_GUI.py
@@ -10,8 +10,8 @@ LIGHT_GREY = (240, 240, 240)
 DARK_GREEN = (80, 180, 80)
 
 # Screen parameters
-screen_width = 1000
-screen_height = 700
+screen_width = 1920
+screen_height = int(screen_width / (16/9))
 
 WIN = pygame.display.set_mode(
     [screen_width, screen_height])  # Create the window
@@ -25,27 +25,35 @@ class Player:
         self.player_num = player_num  # player number: 1, 2, 3 or 4
         self.name = name
 
-        # Player surface object of pygame
-        self.y_offset = 0.2 * screen_height/ 3  # The higher the multiplicator, the more longer player surface is
-        self.x_offset = 0.03 * screen_width / 3 # The higher the multiplicator, smaller is the horizontal player surface (reduces from right side)
-        self.size = [screen_width / 3 - self.x_offset,
-                     screen_height / 3 + self.y_offset]  # The size of player Surface
 
-        self.surface = pygame.Surface(self.size)
+        self.padding = 0.03 * screen_width # Padding in pixels
+        padded_screen = {"width": screen_width - 2 * self.padding, 
+                        'height': screen_height - 2 * self.padding}
+
+        self.size = [padded_screen["width"] / 3,
+                     padded_screen["height"] / 3]
+                        
         self.coordinate = {
-            1: (screen_width / 3 + self.x_offset/2, 
-                screen_height / 3 * 2 - self.y_offset - self.x_offset),
-            2: (screen_width / 3 * 2 + 0, 
-                screen_height / 3),
-            3: (screen_width / 3 + self.x_offset/2, self.x_offset),
-            4: (self.x_offset, screen_height / 3),
+            1: (screen_width/3 + self.padding / 2,
+                screen_height/3 * 2),
+
+            2: (screen_width/3 * 2,
+                screen_height/3 + self.padding / 2),
+
+            3: (screen_width/3 + self.padding / 2,
+                self.padding),
+
+            4: (self.padding,
+                screen_height/3 + self.padding / 2)
         }
+
         # Gaps between cards on player's surface
-        self.v_gap = 0.02 * self.size[1]
-        self.h_gap = 0.13 * self.size[0]
+        self.v_gap = 0.05 * self.size[1]
+        self.h_gap = 0.05 * self.size[0]
 
         self.score = 0
 
+        self.surface = pygame.Surface(self.size)
         self.surface.fill(LIGHT_GREY) # Background color for players
 
     # NOTE: Temporary just to show the UI for now
@@ -60,8 +68,8 @@ class Player:
         for y in range(3):
             for x in range(4):
                 self.surface.blit(card.surface, [24 + card.surface.get_width() * x + self.h_gap * x,
-                                                 10 + self.y_offset + card.surface.get_height() * y + self.v_gap * y])
-        border = pygame.Rect((24, self.y_offset + 10),
+                                                 10 + self.padding + card.surface.get_height() * y + self.v_gap * y])
+        border = pygame.Rect((24, self.padding + 10),
                              (card.size[0] * 4 + self.h_gap * 3, card.size[1] * 3 + self.v_gap * 2))
         pygame.draw.rect(self.surface, BLACK, border, 1)
 
